7c9245c1b0e316510cc8702c3748933a84d34f61
weavelol <deniss.shutov98@gmail.com>
2022-02-26 23:39:26 +0200

    Documentation for ApiService

--- ClientServices/Interfaces/IApiService.cs
+++ ClientServices/Interfaces/IApiService.cs
@@ -1,9 +1,42 @@
 ﻿namespace ClientServices.Interfaces {
+    /// <summary>
+    /// Used to make http requests to api
+    /// </summary>
     public interface IApiService {
+        /// <summary>
+        /// Get request of entities of required type
+        /// </summary>
+        /// <typeparam name="T">required type of entities</typeparam>
+        /// <param name="url">api url</param>
+        /// <returns>entities collection of required type</returns>
         public Task<List<T>> GetEntitiesAsync<T>(string url);
+
+        /// <summary>
+        /// Get request of entity of required  type
+        /// </summary>
+        /// <typeparam name="T">required type of entity</typeparam>
+        /// <param name="url">api url</param>
+        /// <returns>entity of required type</returns>
         public Task<T> GetEntityAsync<T>(string url);
+
+        /// <summary>
+        /// Post request
+        /// </summary>
+        /// <param name="url">api url</param>
+        /// <param name="entity">object to post</param>
         public Task CreateEntityAsync(string url, object entity);
+
+        /// <summary>
+        /// Update request
+        /// </summary>
+        /// <param name="url">api url</param>
+        /// <param name="entity">entity to update</param>
         public Task UpdateEntityAsync(string url, object entity);
+
+        /// <summary>
+        /// Delete request
+        /// </summary>
+        /// <param name="url">api url</param>
         public Task DeleteAsync(string url);
     }
 }
--- ClientServices/Services/ApiService.cs
+++ ClientServices/Services/ApiService.cs
@@ -2,6 +2,7 @@
 using ClientServices.Interfaces;
 
 namespace ClientServices.Services {
+    /// <inheritdoc/>
     public class ApiService : IApiService {
         private readonly HttpClient http;
 
@@ -9,18 +10,27 @@ namespace ClientServices.Services {
             this.http = http;
         }
 
+        /// <inheritdoc/>
         public async Task<List<T>> GetEntitiesAsync<T>(string url) {
             return await http.GetFromJsonAsync<List<T>>(url);
         }
+
+        /// <inheritdoc/>
         public async Task<T> GetEntityAsync<T>(string url) {
             return await http.GetFromJsonAsync<T>(url);
         }
+
+        /// <inheritdoc/>
         public async Task CreateEntityAsync(string url, object entity) {
             await http.PostAsJsonAsync(url, entity);
         }
+
+        /// <inheritdoc/>
         public async Task UpdateEntityAsync(string url, object entity) {
             await http.PutAsJsonAsync(url, entity);
         }
+
+        /// <inheritdoc/>
         public async Task DeleteAsync(string url) {
             await http.DeleteAsync(url);
         }
